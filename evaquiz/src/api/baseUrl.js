e083dfbb98521051d08f71dd9a77d6f10cbee483
vladimir morozov <vmorozov@ot.ru>
2020-06-25 13:59:21 +0300

    редирект если без авторизации

--- evaquiz/src/api/baseUrl.js
+++ evaquiz/src/api/baseUrl.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 
+const instance = axios.create({
+  withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept"
+  }
+});
+
+instance.interceptors.response.use(undefined, error => {
+  if (error.response && error.response.status === 401) {
+    window.location.href = `/`;
+  }
+  return Promise.reject(error.response.data);
+});
+
 export default () => {
-  return axios.create({
-    withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept"
-    }
-  });
+  return instance;
 };
