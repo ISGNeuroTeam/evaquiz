10bbfa51be389d39419163f3d0e6464d0186e060
vladimir morozov <vmorozov@ot.ru>
2020-07-10 18:22:59 +0300

    add nav in header

--- evaquiz/src/components/Header.vue
+++ evaquiz/src/components/Header.vue
@@ -1,6 +1,36 @@
 <template>
   <v-app-bar light app color="primary" class="link-conteiner">
-    <v-toolbar-title> <a href="/" class="logo">EVA</a> </v-toolbar-title>
+    <v-toolbar-title class="logo">EVA</v-toolbar-title>
+    <v-toolbar-title>
+      <v-tooltip bottom color="#41C4FF">
+        <template v-slot:activator="{ on }">
+          <v-icon
+            color="white"
+            class="header_icon"
+            v-on="on"
+            @click="clickHome"
+          >
+            mdi-home-variant-outline
+          </v-icon>
+        </template>
+        <span>На главную</span>
+      </v-tooltip>
+    </v-toolbar-title>
+    <v-toolbar-title>
+      <v-tooltip bottom color="#41C4FF">
+        <template v-slot:activator="{ on }">
+          <v-icon
+            color="white"
+            class="header_icon"
+            v-on="on"
+            @click="clickBack"
+          >
+            mdi-undo-variant
+          </v-icon>
+        </template>
+        <span>Назад</span>
+      </v-tooltip>
+    </v-toolbar-title>
     <v-toolbar-title>
       <router-link to="/list" class="link-style --first">Список </router-link>
     </v-toolbar-title>
@@ -17,7 +47,15 @@
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  methods: {
+    clickHome() {
+      window.location.href = `/main`;
+    },
+    clickBack() {
+      this.$router.go(-1);
+    }
+  }
 };
 </script>
 <style scoped>
@@ -28,6 +66,7 @@ export default {
   font-size: 20px;
   font-weight: bold;
   text-decoration: none;
+  padding-right: 5px;
 }
 .link-style {
   color: black;
@@ -50,5 +89,10 @@ export default {
 }
 .router-link-active {
   color: white;
+}
+.header_icon {
+  height: 33px;
+  padding-left: 5px;
+  padding-right: 5px;
 }
 </style>
